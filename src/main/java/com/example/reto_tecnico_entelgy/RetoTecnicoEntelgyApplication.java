6949c38c07724248f670fd9fed97fc3156d03884
MiguelKballero1303 <xxdeadman1303@gmail.com>
2025-02-24 20:02:57 -0500

    chore: Creación del proyecto con estructura base

---
+++ src/main/java/com/example/reto_tecnico_entelgy/RetoTecnicoEntelgyApplication.java
@@ -0,0 +1,13 @@
+package com.example.reto_tecnico_entelgy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoTecnicoEntelgyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoTecnicoEntelgyApplication.class, args);
+	}
+
+}
